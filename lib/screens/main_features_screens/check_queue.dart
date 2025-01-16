eb9307adace9743793a60c17d743c732cb533516
ChamikaNiroshan <M.ChamikaNiroshan@gmail.com>
2025-01-16 10:58:01 +0530

    Adde RefreshIndicator
    Adde RefreshIndicator. Now  if user drag down then check queue screen will be refresh.

--- lib/screens/main_features_screens/check_queue.dart
+++ lib/screens/main_features_screens/check_queue.dart
@@ -4,9 +4,27 @@ import 'package:test_flutter1/screens/main_features_screens/subscreens/check_que
 import 'package:test_flutter1/screens/main_features_screens/subscreens/check_queue_content/content_middle.dart';
 import 'package:test_flutter1/screens/main_features_screens/subscreens/check_queue_content/content_top.dart';
 
+class CheckQueue extends StatefulWidget {
+  @override
+  _CheckQueueState createState() => _CheckQueueState();
+}
 
-class CheckQueue extends StatelessWidget {
+class _CheckQueueState extends State<CheckQueue> {
   final QueueService queueService = QueueService();
+  Future<Map<String, dynamic>>? queueData;
+
+  @override
+  void initState() {
+    super.initState();
+    queueData = queueService.getTodayQueueInfo();
+  }
+
+  Future<void> _refreshQueueData() async {
+    setState(() {
+      queueData = queueService.getTodayQueueInfo();
+    });
+    await queueData; // Wait for the data to refresh
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,45 +33,49 @@ class CheckQueue extends StatelessWidget {
         title: Text('Check Queue'),
         backgroundColor: Color(0xFF34A0A4),
       ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: queueService.getTodayQueueInfo(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('No data available'));
-          }
+      body: RefreshIndicator(
+        onRefresh: _refreshQueueData,
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: queueData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData) {
+              return Center(child: Text('No data available'));
+            }
 
-          final data = snapshot.data!;
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ContentTop(
-                  totalPositionsToday: data['totalPositionsToday'] ?? 0,
-                  currentServicingPosition: int.tryParse(data['currentServicingPosition']?.toString() ?? '0') ?? 0,
+            final data = snapshot.data!;
+            return SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ContentTop(
+                      totalPositionsToday: data['totalPositionsToday'] ?? 0,
+                      currentServicingPosition: int.tryParse(data['currentServicingPosition']?.toString() ?? '0') ?? 0,
+                    ),
+                    SizedBox(height: 10),
+                    ContentMiddle(
+                      totalPositionsToday: data['totalPositionsToday'] ?? 0,
+                      userPosition: (data['userPosition']?.toString() ?? 'Not found'),
+                    ),
+                    SizedBox(height: 10),
+                    ContentBottom(
+                      hasReservation: data['hasReservation'] ?? false,
+                      isServiced: data['isServiced'] ?? false,
+                      currentServicingPosition: int.tryParse(data['currentServicingPosition']?.toString() ?? '0') ?? 0,
+                      userPosition: (data['userPosition']?.toString() ?? 'Not found'),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
-                ContentMiddle(
-                  totalPositionsToday: data['totalPositionsToday'] ?? 0,
-                  //hasReservation: data['hasReservation'] ?? false,
-                  userPosition: (data['userPosition']?.toString() ?? 'Not found'),
-
-                ),
-                SizedBox(height: 10),
-                ContentBottom(
-                  hasReservation: data['hasReservation'] ?? false,
-                  isServiced: data['isServiced'] ?? false,
-                  currentServicingPosition: int.tryParse(data['currentServicingPosition']?.toString() ?? '0') ?? 0,
-                  userPosition: (data['userPosition']?.toString() ?? 'Not found'),
-                ),
-              ],
-            ),
-          );
-        },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
