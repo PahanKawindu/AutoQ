1d07a7eb392cdd2696eb6e7fa78010ed1e5ea813
ChamikaNiroshan <M.ChamikaNiroshan@gmail.com>
2025-01-25 10:29:25 +0530

    Fix session management Bug
    Fix session management Bug

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,9 +8,30 @@ import 'login_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
+  Future<void> checkSession(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userRole = prefs.getString('userRole');
+    String? uid = prefs.getString('uid');
+
+    if (uid != null) {
+      // Redirect based on user role
+      if (userRole == 'admin') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreenAdmin()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreenUser()),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => checkSession(context));
     return Scaffold(
       backgroundColor: const Color(0xFF46C2AF), // Teal background color
       body: Column(
