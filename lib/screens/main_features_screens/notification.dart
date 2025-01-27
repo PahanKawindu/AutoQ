77a94dfe3bd57f00e71ebccc9b33abb37c4aa9b1
KavindaChathuranga <kavinda2001chathu@gmai.com>
2025-01-28 04:27:02 +0530

    Change notification screen
    -change appearance

--- lib/screens/main_features_screens/notification.dart
+++ lib/screens/main_features_screens/notification.dart
@@ -55,83 +55,86 @@ class _NotificationScreenState extends State<NotificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xFFE5F7F1),
+        backgroundColor: const Color(0xFFE5E5E5),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Title section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Your Notification History',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
+      body: Container(
+        color: const Color(0xFFE5E5E5), // Set the body background color
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Notification History',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
               ),
             ),
-          ),
-          // Notification list section
-          Expanded(
-            child: FutureBuilder<List<Map<String, dynamic>>>(
-              future: _uid != null ? _fetchNotifications() : Future.value([]),  // Only fetch notifications if UID is loaded
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text('No notifications available.'));
-                } else {
-                  List<Map<String, dynamic>> notifications = snapshot.data!;
-                  return ListView.builder(
-                    itemCount: notifications.length,
-                    itemBuilder: (context, index) {
-                      var notification = notifications[index];
-                      var timestamp = notification['timestamp'];
-                      var formattedTime = _formatTimestamp(timestamp);
+            // Notification list section
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: _uid != null ? _fetchNotifications() : Future.value([]),  // Only fetch notifications if UID is loaded
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('No notifications available.'));
+                  } else {
+                    List<Map<String, dynamic>> notifications = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: notifications.length,
+                      itemBuilder: (context, index) {
+                        var notification = notifications[index];
+                        var timestamp = notification['timestamp'];
+                        var formattedTime = _formatTimestamp(timestamp);
 
-                      return Card(
-                        elevation: 5,  // Adds shadow for visual effect
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),  // Rounded corners for the card
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Notification Status
-                              Text(
-                                notification['status'],
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              // Notification Timestamp
-                              Text(
-                                "Received on: $formattedTime",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
+                        return Card(
+                          color: const Color(0xFFE5F7F1),
+                          elevation: 5,  // Adds shadow for visual effect
+                          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),  // Rounded corners for the card
                           ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Notification Status
+                                Text(
+                                  notification['status'],
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                // Notification Timestamp
+                                Text(
+                                  "Received on: $formattedTime",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
